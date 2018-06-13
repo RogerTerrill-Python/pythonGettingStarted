9a6186c03cb0c7ae493fd37fd0bad6958bfbb66e
Roger Dodger <lpaddikt@gmail.com>
2018-06-13 15:19:58 -0700

    Error TypeError

--- gettingstarted.py
+++ gettingstarted.py
@@ -4,9 +4,14 @@ student = {
     "feedback" : None
 }
 
+student["last_name"] = "Kowalski"
+
 try:
     last_name = student["last_name"]
+    numbered_last_name = 3 + last_name
 except KeyError:
     print("Error finding last name")
+except TypeError:
+    print("I can't add these two together!")
 
 print("This code executes!")
