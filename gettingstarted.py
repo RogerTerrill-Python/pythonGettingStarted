9863e92220a01764b1107dccadb2fb6d3f268d5c
Roger Dodger <lpaddikt@gmail.com>
2018-06-15 15:37:01 -0700

    Inheritance and Polymorphism

--- gettingstarted.py
+++ gettingstarted.py
@@ -18,7 +18,15 @@ class Student:
     def get_school_name(self):
         return self.school_name
 
-# mark = Student("Mark")
-# print(mark)
+class HighSchoolStudent(Student):
+    school_name = "Springfield High School"
+    def get_school_name(self):
+        return "This is a high school student"
 
-print(Student.school_name)
+    def get_name_capitalize(self):
+        original_value = super().get_name_capitalize()
+        return original_value + "-HS"
+
+james = HighSchoolStudent("james")
+print(james.get_school_name())
+print(james.get_name_capitalize())
