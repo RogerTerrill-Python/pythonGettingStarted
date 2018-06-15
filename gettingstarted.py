004ac0c5477db8104582914ccb06f1b21ccc6876
Roger Dodger <lpaddikt@gmail.com>
2018-06-15 11:22:23 -0700

    Open, Read, Write files

--- gettingstarted.py
+++ gettingstarted.py
@@ -3,7 +3,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student.title()
+        students_titlecase.append(student["name"].title())
     return students_titlecase
 
 def print_students_titlecase():
@@ -14,12 +14,28 @@ def add_student(name, student_id = 332):
     student = {"name": name, "student_id": student_id}
     students.append(student)
 
-def var_args(name, **kwargs):
-    print(name)
-    print(kwargs["description"], kwargs["feedback"])
+def save_file(student):
+    try:
+        f = open("students.txt", "a")
+        f.write(student + "\n")
+        f.close()
+    except Exception:
+        print("Could not save file")
 
-student_list = get_students_titlecase()
+def read_file():
+    try:
+        f = open("students.txt", "r")
+        for student in f.readlines():
+            add_student(student)
+        f.close()
+    except Exception:
+        print("Could not read file")
 
-add_student(name = "Mark", student_id=15)
+read_file()
+print_students_titlecase()
 
-var_args("Mark", description="Loves Python", feedback=None, pluralsight_subscriber=True)
+student_name = input("Enter student name: ")
+student_id = input("Enter student ID: ")
+
+add_student(student_name, student_id)
+save_file(student_name)
