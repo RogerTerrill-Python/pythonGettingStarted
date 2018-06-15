2c2eebf17a7ab33fda4f2aff6dc5c56bbee484f3
Roger Dodger <lpaddikt@gmail.com>
2018-06-15 10:31:43 -0700

    Keyword Arguements

--- gettingstarted.py
+++ gettingstarted.py
@@ -10,10 +10,16 @@ def print_students_titlecase():
     students_titlecase = get_students_titlecase()
     print(students_titlecase)
 
-def add_student(name):
-    students.append(name)
+def add_student(name, student_id = 332):
+    student = {"name": name, "student_id": student_id}
+    students.append(student)
+
+def var_args(name, **kwargs):
+    print(name)
+    print(kwargs["description"], kwargs["feedback"])
 
 student_list = get_students_titlecase()
 
-add_student("Mark")
-print_students_titlecase();
+add_student(name = "Mark", student_id=15)
+
+var_args("Mark", description="Loves Python", feedback=None, pluralsight_subscriber=True)
